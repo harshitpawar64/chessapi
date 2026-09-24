63aa54c7489cbb5d68594f0340bd94683710b69d
Harshit Pawar <hpchess64@gmail.com>
2026-09-24 16:36:31 +0530

    feat(chesscom): add player and streaming platform models

---
+++ src/chessapi/chesscom/models.py
@@ -0,0 +1,27 @@
+import msgspec
+
+
+class StreamingPlatform(msgspec.Struct, kw_only=True, frozen=True):
+    type: str
+    channel_url: str
+
+
+class ChessComPlayer(msgspec.Struct, kw_only=True, frozen=True):
+    avatar: str | None = None
+    player_id: int
+    api_url: str = msgspec.field(name="@id")
+    url: str
+    name: str | None = None
+    username: str
+    title: str | None = None
+    followers: int
+    country: str
+    location: str | None = None
+    last_online: int
+    joined: int
+    status: str
+    is_streamer: bool
+    twitch_url: str | None = None
+    verified: bool
+    league: str | None = None
+    streaming_platforms: list[StreamingPlatform] = []
